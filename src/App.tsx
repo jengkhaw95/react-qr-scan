4203d53a356e4886dad41c4277bc562da0f3e3b8
Khaw Jeng <jengkhaw95@gmail.com>
2023-01-13 13:13:15 +0800

    chore: cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
 import React, { useState } from 'react'
-import { QrReader } from '../dist/esm/index'
-// import { QrReader } from '../packages'
-// import {QrReader} from "@jengkhaw95/react-qr-scan"
+import { QrReader } from '../packages'
 
 function App() {
-    const [disabled, setDisabled] = useState(true)
+  const [disabled, setDisabled] = useState(true)
 
-    return (
-        <div className="App">
-            <button
-                onClick={() => {
-                    setDisabled((p) => !p)
-                }}
-            >
-                TURN {disabled ? 'ON' : 'OFF'}
-            </button>
-            <QrReader
-                disabled={disabled}
-                onResult={(result) => {
-                    console.log(result)
-                }}
-            />
-        </div>
-    )
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setDisabled((p) => !p)
+        }}
+      >
+        TURN {disabled ? 'ON' : 'OFF'}
+      </button>
+      <QrReader
+        disabled={disabled}
+        onResult={(result) => {
+          console.log(result)
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
