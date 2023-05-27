6689d3fd2e6c17d2c4190b8e9c7a1cc544968ee7
Jeng Khaw <jengkhaw95@gmail.com>
2023-05-27 21:25:12 +0800

    format

--- packages/QrReader.tsx
+++ packages/QrReader.tsx
@@ -5,65 +5,62 @@ import { BrowserQRCodeReader, IScannerControls } from '@zxing/browser'
 import type { Result } from '@zxing/library'
 
 interface QrReaderProps {
-    className?: string
-    disabled?: boolean
-    scanDelay?: number
-    onResult?: (result?: Result, error?: Error) => void
-    mediaTrackConstraints?: MediaTrackConstraints
+  className?: string
+  disabled?: boolean
+  scanDelay?: number
+  onResult?: (result?: Result, error?: Error) => void
+  mediaTrackConstraints?: MediaTrackConstraints
 }
 
 export default function QrReader({
-    className = '',
-    disabled = false,
-    scanDelay = 500,
-    onResult = () => {},
-    mediaTrackConstraints = {
-        facingMode: {
-            ideal: 'environment',
-        },
+  className = '',
+  disabled = false,
+  scanDelay = 500,
+  onResult = () => {},
+  mediaTrackConstraints = {
+    facingMode: {
+      ideal: 'environment',
     },
+  },
 }: QrReaderProps) {
-    const controlRef = useRef<IScannerControls | null>(null)
-    const readerRef = useRef<BrowserQRCodeReader | null>(null)
-    const videoElementRef = useRef<HTMLVideoElement>(null)
+  const controlRef = useRef<IScannerControls | null>(null)
+  const readerRef = useRef<BrowserQRCodeReader | null>(null)
+  const videoElementRef = useRef<HTMLVideoElement>(null)
 
-    useEffect(() => {
-        readerRef.current = new BrowserQRCodeReader(undefined, {
-            delayBetweenScanAttempts: scanDelay,
+  useEffect(() => {
+    readerRef.current = new BrowserQRCodeReader(undefined, {
+      delayBetweenScanAttempts: scanDelay,
+    })
+
+    return () => {
+      controlRef.current?.stop()
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!disabled) {
+      readerRef.current
+        ?.decodeFromConstraints(
+          {
+            video: mediaTrackConstraints,
+          },
+          videoElementRef.current ?? undefined,
+          (result, error) => {
+            onResult(result, error)
+          }
+        )
+        .then((controls: IScannerControls) => (controlRef.current = controls))
+        .catch((error: Error) => {
+          console.warn(error)
         })
+    } else {
+      controlRef.current?.stop()
+    }
+  }, [disabled])
 
-        return () => {
-            controlRef.current?.stop()
-        }
-    }, [])
-
-    useEffect(() => {
-        if (!disabled) {
-            readerRef.current
-                ?.decodeFromConstraints(
-                    {
-                        video: mediaTrackConstraints,
-                    },
-                    videoElementRef.current ?? undefined,
-                    (result, error) => {
-                        onResult(result, error)
-                    }
-                )
-                .then(
-                    (controls: IScannerControls) =>
-                        (controlRef.current = controls)
-                )
-                .catch((error: Error) => {
-                    console.warn(error)
-                })
-        } else {
-            controlRef.current?.stop()
-        }
-    }, [disabled])
-
-    return (
-        <section className={className}>
-            <video ref={videoElementRef}></video>
-        </section>
-    )
+  return (
+    <section className={className}>
+      <video ref={videoElementRef}></video>
+    </section>
+  )
 }
--- rollup.config.js
+++ rollup.config.js
@@ -8,30 +8,30 @@ import dts from 'rollup-plugin-dts'
 // import pack from "./package.json";
 
 export default [
-    {
-        input: 'packages/index.ts',
-        output: [
-            {
-                file: 'dist/cjs/index.js',
-                format: 'cjs',
-                sourcemap: true,
-            },
-            {
-                file: 'dist/esm/index.js',
-                format: 'esm',
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            // resolve(),
-            commonjs(),
-            typescript(),
-            terser(),
-        ],
-    },
-    {
-        input: 'dist/esm/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-    },
+  {
+    input: 'packages/index.ts',
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/esm/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      // resolve(),
+      commonjs(),
+      typescript(),
+      terser(),
+    ],
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
 ]
